9902ed171e132c35eec8894a94adb3f0ead5bec9
martín etcheberry <msetcheberry@gmail.com>
2023-08-27 18:37:11 -0300

    Cambio en return del módulo de prueba

--- moduloPrueba.js
+++ moduloPrueba.js
@@ -1,4 +1,4 @@
 //Prueba de crear y exportar un módulo
 exports.caraOCruz = function () {
-    return "Tiraste y salio: " + (Math.round(Math.random()) == 1 ? 'Cara' : 'Seca');
+    return "Tiraste y salio: " + (Math.round(Math.random()) == 1 ? 'Cara' : 'cruz');
 }
